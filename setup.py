44217a7362dca6e52041d87bac61517344f2548e
SANO Masatoshi <snmsts@gmail.com>
2019-04-15 15:23:26 +0800

    add manifest

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
+import os
+
+def read_file(filename):
+    filepath = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
 
 setup(name='lgl',
       version='0.1',
       description='launch python.',
+      long_description=read_file('readme.md'),
+      long_description_content_type="text/markdown",
       url='https://github.com/libgirlenterprise/lgl',
       author='Team Libgirl',
       author_email='team@libgirl.com',
